381baa3d7aa6e27fa73ba93015113d3d15f1f943
Loris Planté <lorisplante@MacBook-Pro-de-Loris.local>
2024-09-02 17:01:04 +0200

    guide page initialized

---
+++ app/forum/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/header";
+
+export default function Forum() {
+  return (
+    <>
+      <Header></Header>
+      <main className="flex min-h-[calc(100vh-110px)] w-full mt-[110px]">
+        <section className="w-full p-mobile sm:p-desktop flex flex-col gap-4">
+          <h1 className="text-3xl sm:text-4xl">Forum</h1>
+        </section>
+      </main>
+    </>
+  );
+}
