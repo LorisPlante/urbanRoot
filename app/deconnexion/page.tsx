ff0dec75ee0b8fe46da4a79b9ec77fbdcf4aa45f
Loris Planté <lorisplante@mbp-de-loris.home>
2024-09-05 21:47:34 +0200

    account disconnect option

--- app/deconnexion/page.tsx
+++ app/deconnexion/page.tsx
@@ -11,7 +11,7 @@ export default function Connexion() {
           <h1 className="text-3xl">Vous êtes déconnecté</h1>
 
           <a href="/" className="bg-lightGreen font-bold hover:bg-darkGreen hover:text-secondary transition-all duration-300 block mx-auto w-fit px-4 py-2 rounded">
-            Retourner à l'accueil
+            {`Retourner à l'accueil`}
           </a>
           <a href="/connexion" className="bg-lightGreen font-bold hover:bg-darkGreen hover:text-secondary transition-all duration-300 block mx-auto w-fit px-4 py-2 rounded">
             Se reconnecter
